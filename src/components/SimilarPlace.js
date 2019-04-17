b64f4578e754c2f3d0bbf2bda280b548b645ae0f
KadTakhmina <43410327+KadTakhmina@users.noreply.github.com>
2019-04-17 13:01:51 +0600

    new styles for stars in similar place

--- src/components/SimilarPlace.js
+++ src/components/SimilarPlace.js
@@ -12,23 +12,23 @@ class SimilarPlace extends Component {
         <div className = "col col-lg-4 similar-place-info">
             <img className = "similar-place-img" alt = "AlText"
                  src = "https://images.unsplash.com/photo-1521834029104-b056ecebbb05?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=669&q=80"/>
-            <Rater total={5} rating={3} >
-				<i class="far fa-star "></i>
+            <Rater className="rating-star-block" total={5} rating={3} >
+			
 				</Rater>
             <p className = "similar-place-name">"Another rest 1"</p>
         </div>
         <div className = "col col-lg-4 similar-place-info">
             <img className = "similar-place-img" alt = "AlText"
              src = "https://images.unsplash.com/photo-1521834029104-b056ecebbb05?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=669&q=80"/>
-            <Rater total={5} rating={3} >
-				<i class="far fa-star "></i>
+            <Rater className="rating-star-block" total={5} rating={3} >
+				
 				</Rater>
             <p className = "similar-place-name">"Another rest 2"</p>
         </div>
         <div className = "col col-lg-4 similar-place-info">
             <img className = "similar-place-img" alt = "AlText"
              src = "https://images.unsplash.com/photo-1521834029104-b056ecebbb05?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=669&q=80"/>
-          <Rater total={5} rating={3} interactive={false}>
+          <Rater className="rating-star-block" total={5} rating={3} interactive={false}>
 				</Rater>
             <p className = "similar-place-name">"Another rest 3"</p>
         </div>
--- src/pages/RaitingsPage.js
+++ src/pages/RaitingsPage.js
@@ -1,9 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-
 import {Layout,RaitingNav,RaitingCard} from "../components";
-
-
 
 class RaitingsPage extends Component {
  constructor() {
