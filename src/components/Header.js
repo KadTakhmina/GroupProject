42e6957dbe4e1d2cdc46d5c3283d115d144d0b01
Tavita <menashetavita@gmail.com>
2019-04-19 15:31:48 +0600

    fndjfn

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,19 +3,8 @@ import React, { Component } from 'react';
 import HeaderInput from "./HeaderInput";
 import User from "./User";
 import userPhoto from "../img/user.svg"
-<<<<<<< HEAD
 // import "./styles/HeaderBg.css"
-
-=======
-<<<<<<< HEAD
-// import "./styles/HeaderBg.css"
-
-import "./styles/Header.css"
-=======
-import "./styles/Header.css"
-
->>>>>>> 0ebc3056f1cb95b066de135ad5e285d67386f2bd
->>>>>>> 17e8b7f92fb4faeceb5c49d6e51194ac52e9a0a9
+import "./styles/Header.css";
 
 class Header extends Component {
   constructor() {
@@ -32,25 +21,14 @@ class Header extends Component {
         <div className=" container row  ">
         <img className = "logo col-2"
         alt ="logo"/>
-<<<<<<< HEAD
         <HeaderInput />
-=======
-<<<<<<< HEAD
-        <HeaderInput />     
-=======
-
-        <HeaderInput />
-
->>>>>>> 0ebc3056f1cb95b066de135ad5e285d67386f2bd
->>>>>>> 17e8b7f92fb4faeceb5c49d6e51194ac52e9a0a9
-        <User 
-              name = {this.state.userName}
-              lastName = {this.state.userLastName}
-              photo = {this.state.userPhoto}
-               />   
+        <User
+            name = {this.state.userName}
+            lastName = {this.state.userLastName}
+            photo = {this.state.userPhoto}
+        />   
       </div>
       </div>
-      
     );
   }
 }
