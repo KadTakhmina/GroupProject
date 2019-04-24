fbc2d2f327bdc59744f02c9f6de1c40ef24809d9
Tavita <menashetavita@gmail.com>
2019-04-25 01:01:10 +0600

    main changed

--- src/components/Heading.js
+++ src/components/Heading.js
@@ -1,30 +1,22 @@
-import React, { Component } from 'react';
-
+import React, { Component } from "react";
 
 import Button from "./Button";
-import './styles/var.css';
-import './styles/Heading.css';
-
+import "./styles/var.css";
+import "./styles/Heading.css";
 
 class Heading extends Component {
   render() {
     return (
       <div className="Heading container col-12">
-      <div class="heading">
-      <h1 class="text-white">Что вы ищете?</h1>
-      <p class="text-white">Lorem mvksm fndjsfn vdkfm vdskvn sdkfsk cskdf
-      </p>
-<<<<<<< HEAD
-      <div class="arrow-btn">
-      <div class="arrow bounce">
-  <a class="fa fa-arrow-down fa-2x" href="#"></a>
-</div>
-      </div>
-         </div>
-=======
-      
-      <button class="btn btn-primary">Click</button>
->>>>>>> 6241732ca17c6aedccdba1e46f9dfdadce89f571
+        <div class="heading">
+          <h1 class="text-white">Что вы ищете?</h1>
+          <p class="text-white">
+            Lorem mvksm fndjsfn vdkfm vdskvn sdkfsk cskdf
+          </p>
+          <div class="arrow bounce">
+            <a class="fa fa-arrow-down fa-2x" href="#" />
+          </div>
+        </div>
       </div>
     );
   }
--- src/pages/SearchPage.js
+++ src/pages/SearchPage.js
@@ -1,36 +1,26 @@
-import React, { Component ,Fragment } from 'react';
-<<<<<<< HEAD
-// import TypeIt from 'TypeIt';
-import { Layout,VideoBg, Categories, Heading} from "../components";
-=======
-import { Layout,MainInput,VideoBg, Categories, Header, Heading} from "../components";
->>>>>>> 6241732ca17c6aedccdba1e46f9dfdadce89f571
-import '../components/styles/SearchPage.css';
-
+import React, { Component, Fragment } from "react";
+import {
+  Layout,
+  // MainInput,
+  VideoBg,
+  Categories,
+  // Header,
+  Heading
+} from "../components";
+import "../components/styles/SearchPage.css";
 
 class SearchPage extends Component {
- 
   render() {
     return (
       <Fragment>
-        <VideoBg/>
-      <Layout> 
-<<<<<<< HEAD
-        {/* <Header/> */}
-        {/* <Type/> */}
-        <Heading/>
-        <Categories/>
-=======
-        <Header/>
-        <Heading/>
-        
->>>>>>> 6241732ca17c6aedccdba1e46f9dfdadce89f571
-      </Layout>
+        <VideoBg />
+        <Layout>
+          {/* <Header /> */}
+          <Heading />
+        </Layout>
       </Fragment>
-      
     );
   }
 }
 
 export default SearchPage;
-
