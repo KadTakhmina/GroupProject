f20ba0e0fea626ff65f81ee897f710b57f1c0831
KadTakhmina <43410327+KadTakhmina@users.noreply.github.com>
2019-04-24 20:55:46 +0600

    styles and carousel with img

--- src/pages/SearchPage.js
+++ src/pages/SearchPage.js
@@ -1,10 +1,5 @@
 import React, { Component ,Fragment } from 'react';
-<<<<<<< HEAD
-// import TypeIt from 'typeit';
-import { Layout,MainInput,VideoBg, Categories, Header, TypeIt,} from "../components";
-=======
 import { Layout,MainInput,VideoBg, Categories, Header, Heading} from "../components";
->>>>>>> 5dd43b0592e82b98098568c79f99043a58e77d83
 import '../components/styles/SearchPage.css';
 
 
@@ -17,12 +12,7 @@ class SearchPage extends Component {
         {/* <div className="cover"></div>  */}
       <Layout> 
         <Header/>
-<<<<<<< HEAD
-        <TypeIt/>
-        {/* <MainInput/> */}
-=======
         <Heading/>
->>>>>>> 5dd43b0592e82b98098568c79f99043a58e77d83
         <Categories/>
       </Layout>
       </Fragment>
