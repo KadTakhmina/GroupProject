295b950fa6f069658b6e289ea25da8dc08c380aa
KadTakhmina <43410327+KadTakhmina@users.noreply.github.com>
2019-04-25 01:39:25 +0600

    changes

--- src/pages/SearchPage.js
+++ src/pages/SearchPage.js
@@ -1,26 +1,23 @@
-import React, { Component, Fragment } from "react";
-import {
-  Layout,
-  // MainInput,
-  VideoBg,
-  Categories,
-  // Header,
-  Heading
-} from "../components";
-import "../components/styles/SearchPage.css";
+import React, { Component ,Fragment } from 'react';
+import { Layout,MainInput,VideoBg, Categories, Header, Heading} from "../components";
+import '../components/styles/SearchPage.css';
+
 
 class SearchPage extends Component {
+ 
   render() {
     return (
       <Fragment>
-        <VideoBg />
-        <Layout>
-          {/* <Header /> */}
-          <Heading />
-        </Layout>
+        <VideoBg/>
+      <Layout> 
+        <Header/>
+        <Heading/>
+      </Layout>
       </Fragment>
+      
     );
   }
 }
 
 export default SearchPage;
+
